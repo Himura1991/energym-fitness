a21b8f2345f7ac1a13bf9dca6303d3ffbb22036c
martin-dosantos-avantrip <martin.dosantos@avantrip.com>
2017-04-05 17:36:11 -0300

    Cambiando imagenes

--- src/EnergymPageBundle/Controller/DefaultController.php
+++ src/EnergymPageBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace EnergymPageBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+
 class DefaultController extends Controller
 {
     /**
